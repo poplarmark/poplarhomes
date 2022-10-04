ef4ac559c477c36ddc6a770904e0f7399d2e6f1e
M. Mangawang <m.mangawang@pm.me>
2022-10-04 09:04:58 +0800

    Add form validation

---
+++ assets/js/form-validator.js
@@ -0,0 +1,72 @@
+const form = document.getElementById('form__indication');
+const firstname = document.getElementById('input__first-name');
+const lastname = document.getElementById('input__last-name');
+const telephone = document.getElementById('input__telephone');
+const email = document.getElementById('input__email');
+const manageddoors = document.getElementById('input__managed-doors');
+const averagerents = document.getElementById('input__average-rents');
+const monthlydoorfee = document.getElementById('input__monthly-door-fee');
+
+//Show input error messages
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSucces(input)
+    }else {
+        showError(input,'Email is not invalid');
+    }
+}
+
+
+//checkRequired fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input,`${getFieldName(input)} is required`)
+        }else {
+            showSucces(input);
+        }
+    });
+}
+
+
+//check input Length
+function checkLength(input, min ,max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
+    }else {
+        showSucces(input);
+    }
+}
+
+//get FieldName
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+//Event Listeners
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    checkRequired([firstname, lastname, telephone, email, manageddoors, averagerents, monthlydoorfee]);
+    checkLength(firstname,2,35);
+    checkLength(firstname,2,35);
+    checkEmail(email);
+});
