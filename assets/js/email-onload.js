017b2e9704acdd69136bf1c6bf493022ba594525
M. Mangawang <m.mangawang@pm.me>
2022-10-04 22:50:29 +0800

    Add helper js email functions

---
+++ assets/js/email-onload.js
@@ -0,0 +1,16 @@
+<script>
+window.onload = function() {
+    document.getElementById('form__indication').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('service_4994rzd', 'template_eyh0ds8', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
+</script>
