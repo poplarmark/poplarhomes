1c025c6c1b2a909d367d57e453f990e4e6805119
M. Mangawang <m.mangawang@pm.me>
2022-10-04 23:16:48 +0800

    Update email-onload.js

--- assets/js/email-onload.js
+++ assets/js/email-onload.js
@@ -1,16 +1,22 @@
-<script>
-window.onload = function() {
-    document.getElementById('blk-form__inidication-body').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_4994rzd', 'template_eyh0ds8', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
+<script type="text/javascript">
+document
+  .getElementById("blk-form__inidication-body")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const serviceID = "service_4994rzd";
+    const templateID = "template_eyh0ds8";
+
+    // send the email here
+    emailjs.sendForm(serviceID, templateID, this).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("SUCCESS!");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        alert("FAILED...", error);
+      }
+    );
+  });
 </script>
