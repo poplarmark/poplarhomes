8a5e1258f005c3f012da9da155ac8b559e53166d
M. Mangawang <m.mangawang@pm.me>
2022-10-05 19:30:52 +0800

    Update phone input formatter feature

--- assets/js/format-phone.js
+++ assets/js/format-phone.js
@@ -1,6 +1,28 @@
 <script type="text/javascript">
-const phone = document.querySelectorAll("#modal__input-phone, #input__telephone")
+const telephone = document.getElementById("input__telephone")
+telephone.oninput = (e) => {
+  e.target.value = autoFormatPhoneNumber(e.target.value)
+}
 
+function autoFormatPhoneNumber(phoneNumberString) {
+  try {
+    var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+    var match = cleaned.match(/^(1|)?(\d{0,3})?(\d{0,3})?(\d{0,4})?$/);
+    var intlCode = match[1] ? "+1 " : "";
+    return [intlCode, 
+            match[2] ? "(": "",
+            match[2], 
+            match[3] ? ") ": "",
+            match[3],
+            match[4] ? "-": "",
+            match[4]].join("")
+    
+  } catch(err) {
+    return "";
+  }
+}
+
+const phone = document.getElementById("modal__input-phone")
 phone.oninput = (e) => {
   e.target.value = autoFormatPhoneNumber(e.target.value)
 }
@@ -22,4 +44,5 @@ function autoFormatPhoneNumber(phoneNumberString) {
     return "";
   }
 }
+
 </script>
