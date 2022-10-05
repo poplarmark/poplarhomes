6e2d00d250e915fc98681a059fd9baf9f5c7eedb
M. Mangawang <m.mangawang@pm.me>
2022-10-05 19:11:01 +0800

    Update phone input formatter feature

--- assets/js/format-phone.js
+++ assets/js/format-phone.js
@@ -1,5 +1,5 @@
 <script type="text/javascript">
-const phone = document.getElementById("input-phone")
+const phone = document.getElementByClassName("input-phone")
 
 phone.oninput = (e) => {
   e.target.value = autoFormatPhoneNumber(e.target.value)
