e05d2718f89b1e3b9f265335be37a22eb2c17f7b
M. Mangawang <m.mangawang@pm.me>
2022-10-06 20:23:58 +0800

    Update form input validators

--- assets/js/form-validator.js
+++ assets/js/form-validator.js
@@ -1,21 +1,74 @@
 <script>
-$(document).ready(function(){
-    $('.required').blur(function(){
-        
-        if($(this).val() != ''){
-            $(this).removeClass('add-border-red');
-        }else {
-            $(this).addClass('add-border-red');
-        }      
-    });
+const form = document.getElementById('form__indication');
+const username = document.getElementById('username');
+const businessName = document.getElementById('business-name');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const position = document.getElementById('position');
+const mobile = document.getElementById('telephone');
+const email = document.getElementById('email');
+const managedDoors = document.getElementById('managed-doors');
+const averageRents = document.getElementById('average-rents');
+const monthlyDoorFee = document.getElementById('monthly-door-fee');
+const cities = document.getElementById('cities');
 
-    $('.bootstrap-tagsinput').blur(function(){
-        
-        if($(this).val() != ''){
-            $(this).removeClass('add-border-red');
+//Show input error messages
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const form_alert = formControl.querySelector('.form-alert');
+    form_alert.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSucces(input)
+    }else {
+        showError(input,'Email is not valid');
+    }
+}
+
+//check phone is valid
+function checkMobile(input) {
+    const re = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/;
+    if(re.test(input.value.trim())) {
+        showSucces(input)
+    }else {
+        showError(input,'Phone is not invalid');
+    }
+}
+
+
+//checkRequired fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input,`${getFieldName(input)} is required`)
         }else {
-            $(this).addClass('add-border-red');
-        }      
+            showSucces(input);
+        }
     });
+}
+
+//get FieldName
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//Event Listeners
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    checkRequired([businessName, firstName, lastName, position, email, managedDoors, averageRents, monthlyDoorFee, cities]);
+    checkEmail(email);
+    checkMobile(mobile);
 });
 </script>
