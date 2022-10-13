4d01a9541fdbc3b2adbbf0f85efc2684ee76d754
M. Mangawang <m.mangawang@pm.me>
2022-10-13 10:58:23 +0800

    Update hubspot.js

--- assets/js/hubspot-form.js
+++ assets/js/hubspot-form.js
@@ -61,8 +61,10 @@ $('form[action^="https://api.hsforms.com"]').each(function (i) { // intercept fo
                         const parent = $(e.target).parent()
                         // parent.children("form").css("display", "none") // hide form
                         parent.children(".modal-success").css("display", "block").html(response.inlineMessage) // replace .w-form-done with your own form done section
+                        console.log('response success 1')
                     } else if (response.redirectUri) {
                         window.location.href = response.redirectUri
+                        console.log('response success 2')
                     }
                 } else {
                     console.log('response but no inlineMessage or redirectUri')
