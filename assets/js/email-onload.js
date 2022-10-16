8eed36d1d29ac3703cac641fd27301be7287c89d
Mark Lawrence Laroza Mangawang <marklawrence@poplarhomes.com>
2022-10-16 20:39:00 +0800

    Remove empty spaces in email-onload.js

--- assets/js/email-onload.js
+++ assets/js/email-onload.js
@@ -1,15 +1,11 @@
 <script type="text/javascript">
-
 // document.getElementById('input__date').value = Date();
-
 document
   .getElementById("form-body--indication")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-
     const serviceID = "service_4994rzd";
     const templateID = "template_eyh0ds8";
-
     // send the email here
     emailjs.sendForm(serviceID, templateID, this).then(
       (response) => {
