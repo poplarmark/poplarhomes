4561fb787a7b830862c7249c841ea96de001b9fb
M. Mangawang <m.mangawang@pm.me>
2022-10-16 10:33:33 +0800

    Update custom form submit js

--- assets/js/custom-form-submit.js
+++ assets/js/custom-form-submit.js
@@ -1,1 +1,47 @@
-<script type="text/javascript">$(document).off("submit"),$("form").submit(function(t){t.preventDefault();let a=$(this),i=$("[type=submit]",a),e=i.val(),o=i.attr("data-wait"),s=a.attr("method"),l=a.attr("action"),r=a.attr("data-redirect"),f=a.serialize();o&&i.val(o),$.ajax(l,{data:f,method:s}).done(t=>{if(r){window.location=r;return}a.siblings(".w-form-done").show().siblings(".w-form-fail").hide()}).fail(t=>{a.siblings(".w-form-done").hide().siblings(".w-form-fail").show()}).always(()=>{i.val(e)})});</script>
+var Webflow = Webflow || [];
+Webflow.push(function() {  
+  // unbind webflow form handling (keep this if you only want to affect specific forms)
+  $(document).off('submit');
+
+  /* Any form on the page */
+  $('#form__contact-us').submit(function(e) {
+    e.preventDefault();
+
+  	const $form = $(this); // The submitted form
+    const $submit = $('[type=submit]', $form); // Submit button of form
+    const buttonText = $submit.val(); // Original button text
+    const buttonWaitingText = $submit.attr('data-wait'); // Waiting button text value
+    const formMethod = $form.attr('method'); // Form method (where it submits to)
+    const formAction = $form.attr('action'); // Form action (GET/POST)
+    const formRedirect = $form.attr('data-redirect'); // Form redirect location
+    const formData = $form.serialize(); // Form data
+    
+    // Set waiting text
+    if (buttonWaitingText) {
+      $submit.val(buttonWaitingText); 
+    }
+    
+    $.ajax(formAction, {
+    	data: formData,
+      method: formMethod
+    })
+    .done((res) => {
+      // If form redirect setting set, then use this and prevent any other actions
+      if (formRedirect) { window.location = formRedirect; return; }
+
+    	$form
+      	.hide() // optional hiding of form
+    		.siblings('.w-form-done').show() // Show success
+      	.siblings('.w-form-fail').hide(); // Hide failure
+    })
+    .fail((res) => {
+      $form
+      	.siblings('.w-form-done').hide() // Hide success
+    	  .siblings('.w-form-fail').show(); // show failure
+    })
+    .always(() => {
+      // Reset text
+      $submit.val(buttonText);
+    });
+  });
+});
