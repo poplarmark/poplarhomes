88c557addc082b98f30092eebb0a851222d20968
Mark Lawrence Laroza Mangawang <marklawrence@poplarhomes.com>
2022-10-17 14:44:42 +0800

    Update email-onload.js

--- dist/assets/js/email-onload.js
+++ dist/assets/js/email-onload.js
@@ -1,4 +1,4 @@
 <script>
-document.getElementById("input__date").value=Date(),document.getElementById("form-body--indication").addEventListener("submit",(function(e){e.preventDefault();emailjs.sendForm("service_4994rzd","template_eyh0ds8",this).then((e=>{console.log("SUCCESS!",e.status,e.text)}),(e=>{console.log("FAILED...",e)}))}));
+document.getElementById("form-body--indication").addEventListener("submit",(function(e){e.preventDefault();emailjs.sendForm("service_4994rzd","template_eyh0ds8",this).then((e=>{console.log("SUCCESS!",e.status,e.text)}),(e=>{console.log("FAILED...",e)}))}));
 //# sourceMappingURL=email-onload.js.map
 </script>
