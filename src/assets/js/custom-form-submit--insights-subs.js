96c8f82ace90e8e1169a55c5cf32ff1e6c7c1cc8
Mark Lawrence Laroza Mangawang <marklawrence@poplarhomes.com>
2022-10-28 23:58:12 +0800

    Update insights subs script

--- src/assets/js/custom-form-submit--insights-subs.js
+++ src/assets/js/custom-form-submit--insights-subs.js
@@ -1,7 +1,10 @@
 $('#owner').click(function() {
     $('#subscriber_type').val('owner');
+    $('#content_type').text('New owner content will be delivered to you every week.');
+
 });
 
 $('#renter').click(function() {
     $('#subscriber_type').val('renter');
+    $('#content_type').text('New renter content will be delivered to you every week.');
 });
