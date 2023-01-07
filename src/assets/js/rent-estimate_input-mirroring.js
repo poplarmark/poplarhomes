85a0f62f2d22af630f06d62cef26cc5c45dbf3b4
M. Mangawang <m.mangawang@pm.me>
2023-01-07 20:19:33 +0800

    Initialize rent-estimate scripts

---
+++ src/assets/js/rent-estimate_input-mirroring.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".mapboxgl-ctrl-geocoder--input").change(function () {
+    $('input[name="location"]').val($(this).val());
+  });
+}),
+  $(document).ready(function () {
+    $('input[name="postal_code"]').change(function () {
+      $('input[name="zipcode"]').val($(this).val());
+    });
+  }),
+  $(document).ready(function () {
+    $('input[name="rent_bedroom"]').change(function () {
+      $('input[name="bedrooms"]').val($(this).val());
+    });
+  }),
+  $(document).ready(function () {
+    $('input[name="rent_bathroom"]').change(function () {
+      $('input[name="bathrooms"]').val($(this).val());
+    });
+  }),
+  $(document).ready(function () {
+    $("#property-type").change(function () {
+      $('input[name="property_type"]').val(this.value);
+    });
+  })
