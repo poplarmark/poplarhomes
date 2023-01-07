d617df2f5f9ee6419e75f040ba087f3393e4cb65
M. Mangawang <m.mangawang@pm.me>
2023-01-07 22:27:05 +0800

    Update phone formatter for homepage

--- src/assets/js/format-phone__page-main.js
+++ src/assets/js/format-phone__page-main.js
@@ -20,3 +20,26 @@ function autoFormatPhoneNumber(phoneNumberString) {
     return "";
   }
 }
+
+const phone__subsidiary = document.getElementById("subsidiary_input-phone");
+phone__subsidiary.oninput = (e) => {
+  e.target.value = autoFormatPhoneNumber(e.target.value)
+}
+
+function autoFormatPhoneNumber(phoneNumberString) {
+  try {
+    var cleaned = ("" + phoneNumberString).replace(/\D/g, "");
+    var match = cleaned.match(/^(1|)?(\d{0,3})?(\d{0,3})?(\d{0,4})?$/);
+    var intlCode = match[1] ? "+1 " : "";
+    return [intlCode, 
+            match[2] ? "(": "",
+            match[2], 
+            match[3] ? ") ": "",
+            match[3],
+            match[4] ? "-": "",
+            match[4]].join("")
+    
+  } catch(err) {
+    return "";
+  }
+}
