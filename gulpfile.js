bf2874df0ffc889914e173bac4f481614b965a0d
M. Mangawang <m.mangawang@pm.me>
2023-01-13 07:07:48 +0800

    Remove form input ouline when focused-visible

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
 const jsPath = 'src/assets/js/**/*.js';
-const cssPath = 'src/assets/css/components/form_insights/form_insights.css';
+const cssPath = 'src/assets/css/global.css';
 
 function jsBundler() {
     return src(jsPath)
