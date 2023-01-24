fa2d125c1f103ee01a92e6a40b7a5bd4e5f40063
M. Mangawang <m.mangawang@pm.me>
2023-01-24 10:41:03 +0800

    Update homeowners.css and geomarket.css

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
 const jsPath = 'src/assets/js/**/*.js';
-const cssPath = 'src/assets/css/pages/homeowners/homeowners.css';
+const cssPath = 'src/assets/models/**/*.css';
 
 function jsBundler() {
     return src(jsPath)
