a67d188c703cd0b75f8d654b82a0c1fe6d26eece
M. Mangawang <m.mangawang@pm.me>
2023-01-29 13:29:12 +0800

    Add table-pricing.css

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
 const jsPath = 'src/assets/js/**/*.js';
-const cssPath = 'src/assets/css/pages/mna/mna.css';
+const cssPath = 'src/assets/css/components/table-pricing/table-pricing.css';
 
 function jsBundler() {
     return src(jsPath)
