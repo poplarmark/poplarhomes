7c7e93347496d816c6b6fe4bcd4ec0c267451870
M. Mangawang <m.mangawang@pm.me>
2023-02-01 21:33:51 +0800

    Add renters-estimate.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const autoprefixer = require('autoprefixer');
 const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
-const jsPath = 'src/assets/js/**/*.js';
+const jsPath = 'src/assets/js/components/form_renters-estimate/renters-estimate.js';
 const cssPath = 'src/assets/css/**/*.css';
 
 function jsBundler() {
---
+++ src/assets/js/components/form_renters-estimate/renters-estimate.js
@@ -0,0 +1,33 @@
+var options = {
+  componentRestrictions: { country: "us" },
+};
+var gpaInput = document.getElementById("renters-estimate_input-location");
+var autocomplete = new google.maps.places.Autocomplete(gpaInput, options);
+
+function showRentals() {
+  var geocoder = new google.maps.Geocoder();
+  var address = gpaInput.value;
+  var beds = document.getElementById("renters-estimate_input-bedroom").value;
+  var baths = document.getElementById("renters-estimate_input-bathroom").value;
+  var property_type = document.getElementById("property-type").value;
+  geocoder.geocode({ address: address }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+
+      console.log(
+        "latitude: ",
+        latitude,
+        "longitude: ",
+        longitude,
+        "beds: ",
+        beds,
+        "baths: ",
+        baths,
+        "property type: ",
+        property_type
+      );
+      location.href = `https://www.poplarhomes.com/rental-properties-listing?beds=${beds}&lat=${latitude}&lng=${longitude}`;
+    }
+  });
+}
