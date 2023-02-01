1a4fe9e1aa755490b402a8cebde3a8710a70448f
M. Mangawang <m.mangawang@pm.me>
2023-02-01 23:12:44 +0800

    Update renters-estimate script and stylings

--- src/assets/js/components/form_renters-estimate/renters-estimate.js
+++ src/assets/js/components/form_renters-estimate/renters-estimate.js
@@ -31,3 +31,8 @@ function showRentals() {
     }
   });
 }
+
+window.addEventListener("load", function () {
+  var child = document.getElementsByClassName("pac-container")[0];
+  jQuery(child).detach().appendTo("#input_wrap-location");
+});
