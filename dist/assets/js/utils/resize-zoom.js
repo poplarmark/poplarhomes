9153f3024246f4f171e2cb76c78931b31df2339d
M. Mangawang <m.mangawang@pm.me>
2023-02-01 19:27:02 +0800

    Update resize-zoom.js

--- dist/assets/js/utils/resize-zoom.js
+++ dist/assets/js/utils/resize-zoom.js
@@ -1,4 +1,4 @@
 <script>
-jQuery(document).ready((function(){function n(){(window.innerWidth>0?window.innerWidth:document.documentElement.clientWidth)>992?(document.body.style.zoom="90%",console.log("running on desktop viewport")):(document.body.style.zoom="100%",console.log("running on mobile viewport"))}window.onresize=n,n()}));
+jQuery(document).ready((function(){function n(){(window.innerWidth>0?window.innerWidth:document.documentElement.clientWidth)>=992?(document.body.style.zoom="90%",console.log("running on desktop viewport")):(document.body.style.zoom="100%",console.log("running on mobile viewport"))}window.onresize=n,n()}));
 //# sourceMappingURL=resize-zoom.js.map
 </script>
--- src/assets/js/utils/resize-zoom.js
+++ src/assets/js/utils/resize-zoom.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
     function resizeForm(){
         var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
-        if(width > 992){
+        if(width >= 992){
         	document.body.style.zoom = "90%";
             console.log("running on desktop viewport")
         } else {
