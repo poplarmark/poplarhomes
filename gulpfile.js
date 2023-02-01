4865891a09a53e314a921a82b13d35f6a111d8a4
M. Mangawang <m.mangawang@pm.me>
2023-02-01 08:52:02 +0800

    Add "Renting Management" section styling to residents.css

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
 const jsPath = 'src/assets/js/**/*.js';
-const cssPath = 'src/assets/css/pages/homeowners/homeowners.css';
+const cssPath = 'src/assets/css/pages/residents/residents.css';
 
 function jsBundler() {
     return src(jsPath)
