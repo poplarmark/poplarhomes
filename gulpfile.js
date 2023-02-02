807bfc86cdd7fdc3be7d6d9b8b6fba7bb6d229db
M. Mangawang <m.mangawang@pm.me>
2023-02-02 11:10:35 +0800

    Update rent-estimate_input-mirroring.js and rent-estimate_input-functions.js to use Google based API's

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,8 @@ const autoprefixer = require('autoprefixer');
 const sourcemaps = require ('gulp-sourcemaps');
 const {src, series, parallel, dest, watch} = require('gulp');
 // Edit working path here
-const jsPath = 'src/assets/js/**/*.js';
-const cssPath = 'src/assets/css/components/form_renters-estimate/renters-estimate.css';
+const jsPath = 'src/assets/js/components/form_rent-estimate/*.js';
+const cssPath = 'src/assets/css/**/*.css';
 
 function jsBundler() {
     return src(jsPath)
