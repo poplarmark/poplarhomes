36ddf48c69fe5cc6d0ac4732f5d5051cc35839cf
M. Mangawang <m.mangawang@pm.me>
2023-02-02 22:38:49 +0800

    Update mirroring function for rent-estimate

--- src/assets/js/components/form_rent-estimate/rent-estimate_input-mirroring.js
+++ src/assets/js/components/form_rent-estimate/rent-estimate_input-mirroring.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $("#pac_input").on('input', function () {
+  $("#pac_input").bind('input change', function () {
     $('input[name="location"]').val($(this).val());
   });
 }),
