9b7a3eea0453ae743d8b08183ce6f2e83f441a65
M. Mangawang <m.mangawang@pm.me>
2023-02-02 11:18:09 +0800

    Fix maps.js targeting non-existing input ID for autocomplete

--- src/assets/js/utils/maps.js
+++ src/assets/js/utils/maps.js
@@ -11,7 +11,7 @@ function initAutocomplete() {
       mapTypeId: "roadmap",
     });
     // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
+    const input = document.getElementById("pac_input");
     const searchBox = new google.maps.places.SearchBox(input);
   
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
