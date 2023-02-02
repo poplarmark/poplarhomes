622aea0bebddb0f1ac910fabc01e88a9fc2ae280
M. Mangawang <m.mangawang@pm.me>
2023-02-02 18:53:55 +0800

    Update onload-estimate.js

--- dist/assets/js/utils/onload-estimate.js
+++ dist/assets/js/utils/onload-estimate.js
@@ -1,4 +1,4 @@
 <script>
-function appendElement(){var e=document.getElementsByClassName("pac-container")[0];jQuery(e).detach().appendTo("#input_wrap-location")}document.addEventListener("DOMContentLoaded",appendElement);
+window.addEventListener("load",(function(){var e=document.getElementsByClassName("pac-container")[0];jQuery(e).detach().appendTo("#input_wrap-location")}));
 //# sourceMappingURL=onload-estimate.js.map
 </script>
--- src/assets/js/utils/onload-estimate.js
+++ src/assets/js/utils/onload-estimate.js
@@ -1,6 +1,4 @@
-function appendElement() {
+window.addEventListener("load", function () {
   var child = document.getElementsByClassName("pac-container")[0];
   jQuery(child).detach().appendTo("#input_wrap-location");
-}
-
-document.addEventListener("DOMContentLoaded", appendElement);
+});
