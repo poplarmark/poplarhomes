b80a9afc4a21daaf28ab3525133982cd5005eb0b
M. Mangawang <m.mangawang@pm.me>
2023-02-11 11:59:19 +0800

    Update  rent-estimate_input-mirroring.js for new "name" properties

--- src/assets/js/components/form_rent-estimate/rent-estimate_input-mirroring.js
+++ src/assets/js/components/form_rent-estimate/rent-estimate_input-mirroring.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
   $('input[name="postal_code"]').change(function () {
-    $('input[name="zipcode"]').val($(this).val());
+    $('input[name="original_property_zip_code"]').val($(this).val());
   });
 }),
   $(document).ready(function () {
     $('input[name="rent_bedroom"]').change(function () {
-      $('input[name="bedrooms"]').val($(this).val());
+      $('input[name="original_number_of_bedrooms"]').val($(this).val());
     });
   }),
   $(document).ready(function () {
     $('input[name="rent_bathroom"]').change(function () {
-      $('input[name="bathrooms"]').val($(this).val());
+      $('input[name="original_number_of_bathrooms"]').val($(this).val());
     });
   }),
   $(document).ready(function () {
     $("#property-type").change(function () {
-      $('input[name="property_type"]').val(this.value);
+      $('input[name="original_property_type"]').val(this.value);
     });
   });
