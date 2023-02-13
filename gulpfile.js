e669dabeccdc82e0eadb6ca4213f1587e46bfde2
Mark Lawrence Laroza Mangawang <marklawrence@poplarhomes.com>
2023-02-13 22:59:55 +0800

    Update select.css

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const { src, series, parallel, dest, watch } = require("gulp");
 // const cssPath = 'src/assets/css/**/*.css';
 
 const jsPath = "src/assets/js/components/form_renters-estimate/renters-estimate.js";
-const cssPath = "src/assets/css/utils/maps.css";
+const cssPath = "src/assets/css/components/select/select.css";
 
 function jsBundler() {
   return src(jsPath)
