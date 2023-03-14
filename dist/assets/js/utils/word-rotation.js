72f3750eb6cdd7e0d0fb9052cdd758b35333feae
M. Mangawang <m.mangawang@pm.me>
2023-03-14 16:50:26 +0800

    Add ".min" suffix to all minifed javascript files

--- dist/assets/js/utils/word-rotation.js
+++
@@ -1,4 +0,0 @@
-<script>
-setInterval((function(){const t=document.querySelector("span.animated_text[data-show]"),e=t.nextElementSibling||document.querySelector("span.animated_text:first-child"),a=document.querySelector("span.animated_text[data-up]");a&&a.removeAttribute("data-up"),t.removeAttribute("data-show"),t.setAttribute("data-up",""),e.setAttribute("data-show","")}),3e3);
-//# sourceMappingURL=word-rotation.js.map
-</script>
