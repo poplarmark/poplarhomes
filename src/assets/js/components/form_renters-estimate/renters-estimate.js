ce258db50aaad850137839170952699e7a7f51db
M. Mangawang <m.mangawang@pm.me>
2023-03-28 04:23:27 +0800

    Limit renters-estimate form location accessibility via dropdown

--- src/assets/js/components/form_renters-estimate/renters-estimate.js
+++ src/assets/js/components/form_renters-estimate/renters-estimate.js
@@ -1,33 +1,35 @@
-var options = {
+const options = {
+  // types: ['(regions)'],
   componentRestrictions: { country: "us" },
 };
-var gpaInput = document.getElementById("renters-estimate_input-location");
-var autocomplete = new google.maps.places.Autocomplete(gpaInput, options);
+const renters_dropdown = document.getElementById("renters_dropdown");
+const gpaInput = document.getElementById("renters-estimate_input-location");
+const autocomplete = new google.maps.places.Autocomplete(gpaInput, options);
 
 function showRentals() {
-  var geocoder = new google.maps.Geocoder();
-  var address = gpaInput.value;
-  var beds = document.getElementById("renters-estimate_input-bedroom").value;
-  var baths = document.getElementById("renters-estimate_input-bathroom").value;
-  var property_type = document.getElementById("property-type").value;
+  let geocoder = new google.maps.Geocoder();
+  let address = gpaInput.value;
+  let beds = document.getElementById("renters-estimate_input-bedroom").value;
+  let baths = document.getElementById("renters-estimate_input-bathroom").value;
+  let property_type = document.getElementById("property-type").value;
 
   geocoder.geocode({ address: address }, function (results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       var latitude = results[0].geometry.location.lat();
       var longitude = results[0].geometry.location.lng();
 
-      console.log(
-        "latitude: ",
-        latitude,
-        "longitude: ",
-        longitude,
-        "beds: ",
-        beds,
-        "baths: ",
-        baths,
-        "property type: ",
-        property_type
-      );
+      // console.log(
+      //   "latitude: ",
+      //   latitude,
+      //   "longitude: ",
+      //   longitude,
+      //   "beds: ",
+      //   beds,
+      //   "baths: ",
+      //   baths,
+      //   "property type: ",
+      //   property_type
+      // );
       location.href = `https://www.poplarhomes.com/rental-properties-listing?beds=${beds}&lat=${latitude}&lng=${longitude}`;
     }
   });
@@ -35,4 +37,9 @@ function showRentals() {
 window.addEventListener("load", function () {
   var child = document.getElementsByClassName("pac-container")[0];
   jQuery(child).detach().appendTo("#input_wrap-location");
+});
+
+document.getElementById('renters_dropdown').addEventListener('change', function() {
+  // console.log('You selected: ', this.value);
+  gpaInput.value = renters_dropdown.value;
 });
