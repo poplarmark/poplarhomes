70fb864465f3cb96433d0d56ecd41b28228d8d54
M. Mangawang <m.mangawang@pm.me>
2023-04-18 13:15:06 +0800

    Add styling for Investors and Advisors page

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const rename = require("gulp-rename");
 // const jsPath = 'src/assets/js/**/*.js';
 // const cssPath = 'src/assets/css/**/*.css';
 const jsPath ="src/assets/js/utils/wf-custom-form-submit copy.js";
-const cssPath = "src/assets/scss/pages/rent-estimate/rent-estimate.scss";
+const cssPath = "src/assets/scss/pages/location/location.scss";
 
 function jsBundler() {
   return src(jsPath)
