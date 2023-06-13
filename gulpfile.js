bb0652f4960354e897d0509d1c3e09ff77adeca7
M. Mangawang <m.mangawang@pm.me>
2023-06-13 16:43:00 +0800

    Set font-size for all elements under location-licensing text

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ const rename = require("gulp-rename");
 // const jsPath = 'src/assets/js/**/*.js';
 // const cssPath = 'src/assets/css/**/*.css';
 const jsPath ="src/assets/js/utils/wf-custom-form-submit copy.js";
-const cssPath = "src/assets/scss/global.scss";
+const cssPath = "src/assets/scss/pages/location/location.scss";
 
 function jsBundler() {
   return src(jsPath)
